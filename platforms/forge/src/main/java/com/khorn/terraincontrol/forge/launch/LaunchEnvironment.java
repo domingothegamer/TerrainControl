a94e6402a99452ccc02cc05be054ddffe369d4e4
domingothegamer <domingothegamer@gmail.com>
2019-06-30 17:03:35 -0700

    Updated build versions to latest Forge and Sponge Mixin

--- platforms/forge/src/main/java/com/khorn/terraincontrol/forge/launch/LaunchEnvironment.java
+++ platforms/forge/src/main/java/com/khorn/terraincontrol/forge/launch/LaunchEnvironment.java
@@ -7,7 +7,7 @@ final class LaunchEnvironment
 {
     // Would there be a way to extract these from the build.gradle file?
     private static final String MINECRAFT_VERSION = "1.12.2";
-    private static final String MCP_VERSION = "20170624";
+    private static final String MCP_VERSION = "20171003";
     private final String minecraftGradleDir = System.getProperty("user.home") + "/.gradle/caches/minecraft";
 
     String getMcpFile()
